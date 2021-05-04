d27113f8330024f23c1215bbe93ade88b07e7804
ahegelund_KMac <83535003+ahegelund@users.noreply.github.com>
2021-05-04 22:50:10 +0200

    First commit

---
+++ Insomnia.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+chromedriver_location = "/usr/local/bin/chromedriver"
+driver = webdriver.Chrome(chromedriver_location)
+driver.get('https://www.regionh.dk/presse-og-nyt/pressemeddelelser-og-nyheder/Sider/Tilmelding-til-at-modtage-overskydende-vaccine-mod-COVID-19.aspx')
+
+#Page 1 - intro
+
+page_submit = '/html/body/div/form/div[2]/div[3]/input'
+driver.find_element_by_xpath(page_submit).click()
+
+#Page 2 - name
+
+fulde_navn = '//*[@id="t50100775"]'
+driver.find_element_by_xpath(fulde_navn).send_keys('Jakob Jokab')
+driver.find_element_by_xpath(page_submit).click()
+
+#Page 3 - age
+
+alder = '//*[@id="n35965768"]'
+driver.find_element_by_xpath(alder).send_keys('22')
+driver.find_element_by_xpath(page_submit).click()
+
+#Page 4 - address
+
+addresse = '//*[@id="t50088645"]'
+driver.find_element_by_xpath(addresse).send_keys('Hovedbanegården 22')
+driver.find_element_by_xpath(page_submit).click()
+
+#Page 5 - postcode
+
+postcode = '//*[@id="t50088674"]'
+driver.find_element_by_xpath(postcode).send_keys('1111')
+driver.find_element_by_xpath(page_submit).click()
